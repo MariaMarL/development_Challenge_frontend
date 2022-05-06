64d60c1545e23df5353c770003de65407c992623
Maria del Mar Lamilla <lamilla767@gmail.com>
2022-05-06 11:23:04 -0500

    reducer function created

---
+++ src/state/Reducer.jsx
@@ -0,0 +1,16 @@
+function reduce(state, action) {
+    switch (action.type){
+        case 'add-category':
+            return state
+        case 'delete-category':
+            return state
+        case 'add-todo':
+            return state
+        case 'delete-todo':
+            return state
+        case 'edit-todo':
+            return state
+        case 'get-category':
+            return state        
+    }
+}
